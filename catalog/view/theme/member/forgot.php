d01f9c2b09455917d463f5ddabb5ff8ec359b52e
charoenlap <charoenlap88@gmail.com>
2023-02-25 18:34:50 +0700

    update

--- catalog/view/theme/member/forgot.php
+++ catalog/view/theme/member/forgot.php
@@ -2,15 +2,8 @@
     <div class="inner">
         <div class="container">
             <div class="row">
-                <div class="col-sm-4">
+                <div class="col-sm-12">
                     <h1 class="ct-page-header__title">ลืมรหัสผ่าน</h1>
-                </div>
-                <div class="col-sm-8">
-                    <ul class="breadcrumb">
-                        <li><a href="<?php echo route('home');?>">หน้าหลัก</a>
-                        </li>
-                        <li>ลืมรหัสผ่าน</li>
-                    </ul>
                 </div>
             </div>
         </div>
@@ -25,25 +18,9 @@
                 <div class="col-md-6">
                     <h3 class="ct-u-padding-bottom-30">ลืมรหัสผ่าน</h3>
                     <form action="#" method="POST" data-email-subject="Sporta" class="ct-js-validate">
-<<<<<<< HEAD
                         <p class="text-danger">หาต้องการเปลี่ยนรหัสผ่านกรุณาติดต่อเจ้าหน้าที่ โดยสแกนที่ QR Code ด้านล่าง</p>
                         <img src="M_gainfriends_qr.png" style="max-width:100%;height:auto;" alt="">
-                        <a href="https://lin.ee/fMj9QJt" class="btn btn-primary">หรือคลิกที่นี่</a>
-                        <!-- <div class="form-group">
-                            <input type="text" placeholder="เบอร์มือถือ" id="name" name="field" required="required" class="form-control">
-                            <label for="name" class="sr-only">เบอร์มือถือ</label>
-                        </div> -->
-
-                        <!-- <button type="submit" class="btn btn-motive">ลืมรหัสผ่าน</button> -->
-=======
-                        <p class="text-danger">ระบบนี้ยังไม่เปิดให้บริการ หาต้องการเปลี่ยนรหัสผ่านกรุณาติดต่อเจ้าหน้าที่</p>
-                        <div class="form-group">
-                            <input type="text" placeholder="เบอร์มือถือ" id="name" name="field" required="required" class="form-control">
-                            <label for="name" class="sr-only">เบอร์มือถือ</label>
-                        </div>
-
-                        <button type="submit" class="btn btn-motive">ลืมรหัสผ่าน</button>
->>>>>>> 9e0a66df62fd4cd7f7a4c8396bcdefadc687e7a8
+                        <a href="https://lin.ee/UFj4oXI" class="btn btn-primary">หรือคลิกที่นี่</a>
                     </form>
                 </div>
             </div>
